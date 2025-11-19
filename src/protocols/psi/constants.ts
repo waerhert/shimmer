0175882fdf815889cd8ee31239f0db49157bba49
Sven De Waerhert <sven@waerhert.dev>
2025-11-19 17:13:12 +0100

    Improve protocol constants

--- src/protocols/psi/constants.ts
+++ src/protocols/psi/constants.ts
@@ -1,3 +1,1 @@
-export const PROTOCOL_VERSION = '1.0.0'
-export const PROTOCOL_NAME = 'psi'
-export const PSI_TIMEOUT_MS = 30000 // 30 seconds timeout for PSI operations
+export const PSI_TIMEOUT_MS = 3000 // 3 seconds timeout for PSI operations
